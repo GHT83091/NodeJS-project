2acbb7657847f38e0b34545cd9d59ab9f5fea83b
Thanadej N <ght83091@gmail.com>
2022-08-12 19:10:41 +0700

    update code path added

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
-const port = 3002;
+const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
+
+app.set("views","./src/views");
+app.set("view engine","ejs");
 
 app.get("/", (req,res) =>{
 
-    res.send('TEST NODE JS');    // to run test at blowser fill==> http://localhost:3002 or localhost:3002
+    res.render('index', {username: 'GAGA1', customer:["B1", "B2", "B3"]})
 })
 
-app.listen(port, () =>{
-    debug("Listening on port "+ chalk.green(("TCP Port : ") + port));    // if you found ERR_REQUIRE_ESM you have to https://stackoverflow.com/questions/70309135/chalk-error-err-require-esm-require-of-es-
+app.listen(PORT, () =>{
+    debug("Listening on port "+ chalk.green(("TCP Port : ") + PORT));    // if you found ERR_REQUIRE_ESM you have to https://stackoverflow.com/questions/70309135/chalk-error-err-require-esm-require-of-es-
 })
